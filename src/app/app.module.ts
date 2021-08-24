c7b9ffb2adc6d1fee2780eddffde6afe9b12d6ed
Janek <jan.skiba.poczta@gmail.com>
2021-08-24 16:10:25 +0200

    add basic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule,
